a293847d7edcb018aa90a4973b69a8c3d74a34f0
unknown <adamsabela94@gmail.com>
2023-11-13 14:37:59 +0100

    Fixed errors with random generator.

--- Scripts/GameplayCore/Levels/LevelManager.cs
+++ Scripts/GameplayCore/Levels/LevelManager.cs
@@ -16,14 +16,14 @@ public class LevelManager
 	{
 		Level level = new Level();
 		
-		for (int x = 0; x > level.FightList.Lenght;x++)
+		for (int x = 0; x < level.FightList.Length;x++)
 		{
 			Fight fight = new Fight();
-			for (int y = 0; y > fight.EnemyList.Lenght;y++)
+			for (int y = 0; y < fight.EnemyList.Length;y++)
 			{
-				var random_generator = RandomNumberGenerator.new();
-				random_generator.randomize();
-				var random_value = random_generator.randf_range(0,Enemies.Count);
+				var random_generator = new RandomNumberGenerator();
+				random_generator.Randomize();
+				var random_value = random_generator.RandiRange(0,Enemies.Count-1);
 				
 				fight.EnemyList[y] = Enemies[random_value];
 			}
